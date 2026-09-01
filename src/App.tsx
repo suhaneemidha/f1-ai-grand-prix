1fe0de1f5aecb900ed78bd3fb561e46d0a02bd08
suhanee <suhaneemidha30@gmail.com>
2026-09-01 20:06:20 +0530

    feat: connect BFS path to track

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import Track from './components/Track';
 import level1 from './tracks/level1.json';
+import { bfs } from './algorithms/bfs';
 
 function App() {
+  const bfsResult = bfs(
+    level1.grid,
+    level1.start,
+    level1.end
+  );
+
   return (
     <main>
       <h1>F1 AI Grand Prix</h1>
 
       <Track
         grid={level1.grid}
+        start={level1.start}
+        end={level1.end}
+        path={bfsResult.path}
       />
     </main>
   );
